d2eeb4eae2954583600b87581977099b5facb62b
Eduardo Quezada <eduardo602002@gmail.com>
2022-02-26 18:35:30 -0300

    Shops now set the obtained item flag.

--- src/shop.c
+++ src/shop.c
@@ -1059,6 +1059,7 @@ static void BuyMenuTryMakePurchase(u8 taskId)
     {
         if (AddBagItem(tItemId, tItemCount) == TRUE)
         {
+            GetSetItemObtained(tItemId, FLAG_SET_OBTAINED);
             BuyMenuDisplayMessage(taskId, gText_HereYouGoThankYou, BuyMenuSubtractMoney);
             RecordItemPurchase(taskId);
         }
