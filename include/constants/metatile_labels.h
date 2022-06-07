bf916d69751ca3a55c73ef3f62331e79e94bc256
Karl <Karl_Harmer@hotmail.com>
2022-06-07 16:56:48 +0100

    Add Chandra Village door anims

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -399,7 +399,7 @@
 #define METATILE_PetalburgGym_SlidingDoor_Frame4  0x21C
 
 // gTileset_Petalburg
-#define METATILE_Petalburg_Door_BirchsLab   0x249
+#define METATILE_Petalburg_Door_BirchsLab   0x34D
 #define METATILE_Petalburg_Door_Littleroot  0x248
 #define METATILE_Petalburg_Door_Oldale      0x287
 
