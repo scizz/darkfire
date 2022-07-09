3e2ae27fd3987103347f9ceac9fd752942dd78fb
karl <Karl_Harmer@hotmail.com>
2022-07-09 19:50:17 +0100

    Script Chandra Village north exit prevention

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -98,7 +98,7 @@
 #define VAR_LOTAD_SIZE_RECORD                0x404F
 #define VAR_LITTLEROOT_TOWN_STATE            0x4050
 #define VAR_OLDALE_TOWN_STATE                0x4051
-#define VAR_DEWFORD_TOWN_STATE               0x4052 // Unused Var
+#define VAR_CHANDRA_VILLAGE_EXIT_NORTH       0x4052 // Darkfire Var
 #define VAR_LAVARIDGE_TOWN_STATE             0x4053
 #define VAR_CURRENT_SECRET_BASE              0x4054 // was probably allocated for VAR_FALLARBOR_TOWN_STATE at one point
 #define VAR_VERDANTURF_TOWN_STATE            0x4055 // Unused Var
