3208f757b59aa78b3a0016265f205898ca2a64b5
karl <Karl_Harmer@hotmail.com>
2022-07-28 19:24:57 +0100

    Add Chandra Village music

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -540,6 +540,7 @@
 #define PH_NURSE_BLEND              607
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
+#define MUS_CHANDRA_VILLAGE         610
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
--- songs.mk
+++ songs.mk
@@ -1262,3 +1262,6 @@ $(MID_SUBDIR)/se_m_leer.s: %.s: %.mid
 
 $(MID_SUBDIR)/se_dex_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -v100 -P5
+
+$(MID_SUBDIR)/mus_chandra_village.s: %.s: %.mid
+	$(MID) $< $@ -E -G191 -V127
