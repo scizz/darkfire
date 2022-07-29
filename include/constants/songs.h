966351c904298d99e94ca5451cbb5c28de061384
karl <Karl_Harmer@hotmail.com>
2022-07-29 12:01:16 +0100

    Add Route 5 & 6 music

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -553,6 +553,7 @@
 #define MUS_SUBO_CONTROL            620
 #define MUS_SUBO_THEME              621
 #define MUS_ROUTE_3_4               622
+#define MUS_ROUTE_5_6               623
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
