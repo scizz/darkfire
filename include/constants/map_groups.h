2923d986174f46fbe7d8e231bf1e54c38f73138c
karl <Karl_Harmer@hotmail.com>
2022-10-22 22:22:51 +0100

    Add Route 4 wild encounter data

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -1,10 +1,6 @@
 #ifndef GUARD_CONSTANTS_MAP_GROUPS_H
 #define GUARD_CONSTANTS_MAP_GROUPS_H
 
-//
-// DO NOT MODIFY THIS FILE! It is auto-generated from data/maps/map_groups.json
-//
-
 // Map Group 0
 #define MAP_CHANDRA_VILLAGE   (0 | (0 << 8))
 #define MAP_SUNRISE_TOWN      (1 | (0 << 8))
