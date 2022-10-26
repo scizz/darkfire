e95bb06748a77ca5e65ccb618ad50cbe2a9eb191
karl <Karl_Harmer@hotmail.com>
2022-10-26 12:46:50 +0100

    Add surfing dismount ground effect

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1765,6 +1765,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
