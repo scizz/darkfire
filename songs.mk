9663b2bd8a62c55fabb04c59dcc39b75c02c1c0c
W1serV1ser <1prefertheaircauseimanairnomad@gmail.com>
2023-05-10 13:14:53 -0700

    Increased volume of route 7/8 theme

--- songs.mk
+++ songs.mk
@@ -1306,7 +1306,7 @@ $(MID_SUBDIR)/mus_route_5_6.s: %.s: %.mid
 	$(MID) $< $@ -E -G191 -V087
 
 $(MID_SUBDIR)/mus_route_7_8.s: %.s: %.mid
-	$(MID) $< $@ -E -G191 -V08
+	$(MID) $< $@ -E -G191 -V087
 
 $(MID_SUBDIR)/mus_route_9_10.s: %.s: %.mid
 	$(MID) $< $@ -E -G191 -V082
