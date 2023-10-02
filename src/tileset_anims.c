be065743b99c842d084315acc6192933d17c0989
karl <Karl_Harmer@hotmail.com>
2023-10-02 16:01:31 +0100

    Add white flower animation

--- src/tileset_anims.c
+++ src/tileset_anims.c
@@ -44,6 +44,7 @@ static void TilesetAnim_BikeShop(u16);
 static void TilesetAnim_BattlePyramid(u16);
 static void TilesetAnim_BattleDome(u16);
 static void QueueAnimTiles_General_Flower(u16);
+static void QueueAnimTiles_General_WhiteFlower(u16);
 static void QueueAnimTiles_General_Water(u16);
 static void QueueAnimTiles_General_PondWaterEdge(u16);
 static void QueueAnimTiles_General_PondWaterSide(u16);
@@ -109,6 +110,14 @@ const u16 *const gTilesetAnims_General_Flower[] = {
     gTilesetAnims_General_Flower_Frame1,
     gTilesetAnims_General_Flower_Frame2,
     gTilesetAnims_General_Flower_Frame3
+};
+
+const u16 gTilesetAnims_General_WhiteFlower_Frame0[] = INCBIN_U16("data/tilesets/primary/general/anim/white_flower/0.4bpp");
+const u16 gTilesetAnims_General_WhiteFlower_Frame1[] = INCBIN_U16("data/tilesets/primary/general/anim/white_flower/1.4bpp");
+
+const u16 *const gTilesetAnims_General_WhiteFlower[] = {
+    gTilesetAnims_General_WhiteFlower_Frame0,
+    gTilesetAnims_General_WhiteFlower_Frame1
 };
 
 const u16 gTilesetAnims_General_Water_Frame0[] = INCBIN_U16("data/tilesets/primary/general/anim/water/0.4bpp");
@@ -943,6 +952,8 @@ static void TilesetAnim_General(u16 timer)
         QueueAnimTiles_General_WeedT(timer >> 4);
     if (timer % 16 == 10)
         QueueAnimTiles_General_WeedNT(timer >> 4);
+    if (timer % 16 == 11)
+        QueueAnimTiles_General_WhiteFlower(timer >> 4);
 }
 
 static void TilesetAnim_Building(u16 timer)
@@ -955,6 +966,12 @@ static void QueueAnimTiles_General_Flower(u16 timer)
 {
     u16 i = timer % ARRAY_COUNT(gTilesetAnims_General_Flower);
     AppendTilesetAnimToBuffer(gTilesetAnims_General_Flower[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(504)), 4 * TILE_SIZE_4BPP);
+}
+
+static void QueueAnimTiles_General_WhiteFlower(u16 timer)
+{
+    u16 i = timer % ARRAY_COUNT(gTilesetAnims_General_WhiteFlower);
+    AppendTilesetAnimToBuffer(gTilesetAnims_General_WhiteFlower[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(508)), 4 * TILE_SIZE_4BPP);
 }
 
 static void QueueAnimTiles_General_Water(u16 timer)
