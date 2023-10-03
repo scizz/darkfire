467315e71a0812890ba4c9695e2e19e91bb2c48c
karl <Karl_Harmer@hotmail.com>
2023-10-03 14:42:57 +0100

    Script Laveria Mart + R9 Dome Sailor

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1222,7 +1222,7 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493
+#define FLAG_RECEIVED_ROUTE_9_AWAKENING                             0x493 // Darkfire Flag
 
 #define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
 #define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
