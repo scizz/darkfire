a2cbc64d9fece6d38abc24111732a5f37a536fd1
karl <Karl_Harmer@hotmail.com>
2024-01-17 12:56:40 +0000

    Insert Laveria chimney smoke anim

--- src/data/tilesets/headers.h
+++ src/data/tilesets/headers.h
@@ -28,7 +28,7 @@ const struct Tileset gTileset_Rustboro =
     .palettes = gTilesetPalettes_Rustboro,
     .metatiles = gMetatiles_Rustboro,
     .metatileAttributes = gMetatileAttributes_Rustboro,
-    .callback = NULL,
+    .callback = InitTilesetAnim_Rustboro,
 };
 
 const struct Tileset gTileset_Dewford =
