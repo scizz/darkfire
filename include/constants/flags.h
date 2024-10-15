6ea40774e63ebb4a8c3f8119909e8697f7beff9c
karl <Karl_Harmer@hotmail.com>
2024-10-15 18:46:24 +0100

    script

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1277,7 +1277,7 @@
 #define FLAG_ROUTE_11_SAFEGUARD_GIVEN                               0x4C7 // Darkfire Flag
 #define FLAG_ROUTE_5_STONE_LIT                                      0x4C8 // Darkfire Flag
 #define FLAG_ROUTE_5_STONE_2_LIT                                    0x4C9 // Darkfire Flag
-#define FLAG_UNUSED_0x4CA                                           0x4CA // Unused Flag
+#define FLAG_GIVEN_WATERWAY_CITY_SITRUS_BERRY                       0x4CA // Darkfire Flag
 
 #define FLAG_UNUSED_0x4CB                                           0x4CB // Unused Flag
 #define FLAG_UNUSED_0x4CC                                           0x4CC // Unused Flag
